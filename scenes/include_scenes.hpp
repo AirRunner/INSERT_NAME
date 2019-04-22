0c6d9586fbde779944b1d0eb471af2bc5313c285
Narice <vincent.mouillon@gmail.com>
2019-04-22 13:39:02 +0000

    added pragma once to include_scenes.hpp

--- scenes/include_scenes.hpp
+++ scenes/include_scenes.hpp
@@ -1,4 +1,10 @@
+#pragma once
 #include "menus/level_select.hpp"
 #include "menus/main_menu.hpp"
 #include "menus/pause_menu.hpp"
 #include "menus/sandbox_menu.hpp"
+#include "lesson/lesson.hpp"
+#include "lesson/text/text_editor.hpp"
+#include "lesson/text/text_rep.hpp"
+#include "lesson/visual/visual_editor.hpp"
+#include "lesson/visual/visual_rep.hpp"
