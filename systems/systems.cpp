4dc588d0b737268c68dfb568763a7b8a999d52fc
Narice <vincent.mouillon@gmail.com>
2019-04-25 18:44:56 +0000

    added systems for managing buttons

--- systems/systems.cpp
+++ systems/systems.cpp
@@ -21,3 +21,39 @@ void systems::drawEntities(entt::DefaultRegistry* registry)
         }
     );
 }
+
+void systems::drawButtons(entt::DefaultRegistry* registry, const Font& font, float fontSize, float spacing)
+{
+    registry->view<button>().each(
+        [font, fontSize, spacing](auto entity, auto& button)
+        {
+            Color color = GRAY;
+            if(button.selected)
+            {
+                color = LIGHTGRAY;
+            }
+            DrawRectangleRounded(button.rect, 0.3, 10, color);
+            DrawTextRec(font, button.text.c_str(), button.rect, fontSize, spacing, true, button.color);
+        }
+    );
+}
+
+int systems::updateButtons(entt::DefaultRegistry* registry, Vector2 mousePos, bool mouseActive, int selected)
+{
+    int id = selected;
+    registry->view<button>().each(
+        [mousePos, mouseActive, &id](auto entity, auto& button)
+        {
+            if((mouseActive && CheckCollisionPointRec(mousePos, button.rect)) || id == button.id)
+            {
+                button.selected = 1;
+                id = button.id;
+            }
+            else if(id != button.id)
+            {
+                button.selected = 0;
+            }
+        }
+    );
+    return id;
+}
--- systems/systems.hpp
+++ systems/systems.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <chrono>
 #include <thread>
+#include <fstream>
+
 #include <raylib.h>
 #include <raymath.h>
+
 #include <entt.hpp>
-#include <rapidjson.h>
+
+#include <document.h>
+#include <istreamwrapper.h>
+namespace rj = rapidjson;
 
 #include "../components/components.hpp"
+
 
 namespace systems
 {
     void updatePos(entt::DefaultRegistry* registry, float deltaTime);
     void drawEntities(entt::DefaultRegistry* registry);
+    void drawButtons(entt::DefaultRegistry* registry, const Font& font, float fontSize, float spacing);
+    int updateButtons(entt::DefaultRegistry* registry, Vector2 mousePos, bool mouseActive, int selected);
 }
