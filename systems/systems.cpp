80d440c102e9f4f8008e542cb179bf4df5427fdd
dragono123 <vincentlim1@hotmail.fr>
2019-04-27 20:46:24 +0200

    add drawTextPro with alignement options(h only)

--- systems/systems.cpp
+++ systems/systems.cpp
@@ -33,10 +33,159 @@ void systems::drawButtons(entt::DefaultRegistry* registry, const Font& font, flo
                 color = LIGHTGRAY;
             }
             DrawRectangleRounded(button.rect, 0.3, 10, color);
-            DrawTextRec(font, button.text.c_str(), button.rect, fontSize, spacing, true, button.color);
+            // DrawTextRec(font, button.text.c_str(), button.rect, fontSize, spacing, true, button.color);
+            systems::drawTextRecPro(font, button.text.c_str(), button.rect, fontSize, spacing, true, button.color, 0, 0, WHITE, WHITE, 1, 1);
         }
     );
 }
+
+
+
+
+void systems::drawTextRecPro(Font font, const char *text, Rectangle rec, float fontSize, float spacing, bool wordWrap, Color tint, int selectStart, int selectLength, Color selectText, Color selectBack, int halign, int valign)
+{
+
+    int length = strlen(text);
+    int textOffsetX = 0;        // Offset between characters
+    int textOffsetY = 0;        // Required for line break!
+    float scaleFactor = 0.0f;
+
+    int letter = 0;             // Current character
+    int index = 0;              // Index position in sprite font
+
+    scaleFactor = fontSize/font.baseSize;
+
+    enum { MEASURE_STATE = 0, DRAW_STATE = 1 };
+    int state = wordWrap? MEASURE_STATE : DRAW_STATE;
+    int startLine = -1;   // Index where to begin drawing (where a line begins)
+    int endLine = -1;     // Index where to stop drawing (where a line ends)
+
+    for (int i = 0, k = 0; i < length; i++, k++)
+    {
+        int glyphWidth = 0;
+        int next = 1;
+        letter = GetNextCodepoint(&text[i], &next);
+        // NOTE: normally we exit the decoding sequence as soon as a bad byte is found (and return 0x3f)
+        // but we need to draw all of the bad bytes using the '?' symbol so to not skip any we set `next = 1`
+        if(letter == 0x3f) next = 1; 
+        index = GetGlyphIndex(font, letter);
+        i += next - 1;
+
+        if (letter != '\n')
+        {   
+            glyphWidth = (font.chars[index].advanceX == 0)?
+                         (int)(font.chars[index].rec.width*scaleFactor + spacing):
+                         (int)(font.chars[index].advanceX*scaleFactor + spacing);
+        }
+
+        // NOTE: When wordWrap is ON we first measure how much of the text we can draw
+        // before going outside of the `rec` container. We store this info inside
+        // `startLine` and `endLine` then we change states, draw the text between those two
+        // variables then change states again and again recursively until the end of the text
+        // (or until we get outside of the container).
+        // When wordWrap is OFF we don't need the measure state so we go to the drawing
+        // state immediately and begin drawing on the next line before we can get outside
+        // the container.
+        if (state == MEASURE_STATE)
+        {
+            // TODO: there are multiple types of `spaces` in UNICODE, maybe it's a good idea to add support for more
+            // see: http://jkorpela.fi/chars/spaces.html 
+            if ((letter == ' ') || (letter == '\t') || (letter == '\n')) endLine = i;
+
+            if ((textOffsetX + glyphWidth + 1) >= rec.width)
+            {
+                endLine = (endLine < 1)? i : endLine;
+                if (i == endLine) endLine -= next;
+                if ((startLine + next) == endLine) endLine = i - next;
+                state = !state;
+            }
+            else if ((i + 1) == length)
+            {
+                endLine = i;
+                state = !state;
+            }
+            else if (letter == '\n')
+            {
+                state = !state;
+            }
+
+            if (state == DRAW_STATE)
+            {
+                textOffsetX = 0;
+                if(halign != 0)
+                {
+                    char *cptext = new char[endLine - startLine + 2];
+                    for(int j = startLine+1; j <= endLine; j++)
+                        cptext[j - startLine + 1] = text[j];
+                    cptext[endLine + 1] = '\0';
+                    textOffsetX = rec.width - MeasureTextEx(font, cptext, fontSize, spacing).x;
+
+                    delete[] cptext;
+                }
+                if(halign == 1)
+                    textOffsetX /= 2;
+
+                i = startLine;
+                glyphWidth = 0;
+            }
+
+        }
+        else
+        {
+            if (letter == '\n')
+            {
+                if (!wordWrap)
+                {
+                    textOffsetY += (int)((font.baseSize + font.baseSize/2)*scaleFactor);
+                    textOffsetX = 0;
+                }
+            }
+            else
+            {
+                if (!wordWrap && ((textOffsetX + glyphWidth + 1) >= rec.width))
+                {
+                    textOffsetY += (int)((font.baseSize + font.baseSize/2)*scaleFactor);
+                    textOffsetX = 0;
+                }
+
+                if ((textOffsetY + (int)(font.baseSize*scaleFactor)) > rec.height) break;
+
+                //draw selected
+                bool isGlyphSelected = false;
+                if ((selectStart >= 0) && (k >= selectStart) && (k < (selectStart + selectLength)))
+                {
+                    Rectangle strec = {rec.x + textOffsetX-1, rec.y + textOffsetY, (float)glyphWidth, (font.baseSize + font.baseSize/4)*scaleFactor };
+                    DrawRectangleRec(strec, selectBack);
+                    isGlyphSelected = true;
+                }
+
+                //draw glyph
+                if ((letter != ' ') && (letter != '\t'))
+                {
+                    DrawTexturePro(font.texture, font.chars[index].rec,
+                      (Rectangle){ rec.x + textOffsetX + font.chars[index].offsetX*scaleFactor,
+                                   rec.y + textOffsetY + font.chars[index].offsetY*scaleFactor,
+                                   font.chars[index].rec.width*scaleFactor,
+                                   font.chars[index].rec.height*scaleFactor }, (Vector2){ 0, 0 }, 0.0f,
+                                   (!isGlyphSelected)? tint : selectText);
+                }
+            }
+
+            if (wordWrap && (i == endLine))
+            {
+                textOffsetY += (int)((font.baseSize + font.baseSize/2)*scaleFactor);
+                textOffsetX = 0;
+                startLine = endLine;
+                endLine = -1;
+                glyphWidth = 0;
+                state = !state;
+            }
+        }
+
+        textOffsetX += glyphWidth;
+    }
+}
+
 
 int systems::updateButtons(entt::DefaultRegistry* registry, Vector2 mousePos, bool mouseActive, int selected)
 {
