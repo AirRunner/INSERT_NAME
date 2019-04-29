ab7bb3917697104223c871a6eded4e11bb45951b
Narice <vincent.mouillon@gmail.com>
2019-04-29 16:18:33 +0000

    implemented parseText and parseEvent

--- data-layer/jsonParser.cpp
+++ data-layer/jsonParser.cpp
@@ -1,37 +1,77 @@
 #include "jsonParser.hpp"
+#include "../scenes/lesson/lesson.hpp"
 
-
-Event JsonParser::parseLesson() //returns the continue key
+JsonParser::JsonParser()
 {
-    createEntities();
-    parseAnim();
-    updateText();
-    parseTransition();
-    return parseEvent();
+    counter = 0;
 }
 
-void JsonParser::updateText() //updates the text of the scene
+void JsonParser::parseLesson(Lesson& lesson) //returns the continue key
+{
+    createEntities(lesson.doc, lesson.registry);
+    loadRessources(lesson);
+    parseAnim(lesson.doc, lesson.registry);
+    updateText(lesson.doc, lesson.text);
+    parseTransition(lesson);
+    parseEvent(lesson.doc, lesson.nextEvent);
+}
+
+void JsonParser::updateText(rj::Document& doc, std::string& text) //updates the text of the scene
+{
+    if(doc[counter].IsObject())
+    {
+        if(doc[counter]["text"].IsString())
+        {
+            text = doc[counter]["text"].GetString();
+        }
+        else
+        {
+            text = "";
+        }
+    }
+}
+
+void JsonParser::loadRessources(Lesson& lesson) //load the ressources into the cache
 {
 
 }
 
-void JsonParser::createEntities() //creates the appropiate entities
+void JsonParser::createEntities(rj::Document& doc, entt::DefaultRegistry* registry) //creates the appropiate entities
 {
 
 }
 
-void JsonParser::parseAnim() //parses and executes the animations
+void JsonParser::parseAnim(rj::Document& doc, entt::DefaultRegistry* registry) //parses and executes the animations
 {
 
 }
 
-void JsonParser::parseTransition() //parses and execute the transition between two screens
+void JsonParser::parseTransition(Lesson& lesson) //parses and execute the transition between two screens
 {
 
 }
 
-Event JsonParser::parseEvent() //parses and returns the continue key
+void JsonParser::parseEvent(rj::Document& doc, Event& nextEvent) //parses and returns the continue key
 {
-    
-    return Continue;
+    if(doc[counter].IsObject())
+    {
+        if(doc[counter]["event"].IsString())
+        {
+            std::string event = doc[counter]["event"].GetString();
+
+            if(event == "continue")
+            {
+                nextEvent = Continue;
+            }
+            if(event == "block")
+            {
+                nextEvent = Block;
+            }
+        }
+        else
+        {
+            nextEvent = Null;
+        }
+        counter++;
+    }
 }
--- data-layer/jsonParser.hpp
+++ data-layer/jsonParser.hpp
@@ -1,30 +1,49 @@
 #pragma once
 
 #include <iostream>
-#include <rapidjson.h>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <map>
+
 #include <raylib.h>
+#include <raymath.h>
+
+#include <entt.hpp>
+
+#include <document.h>
+#include <istreamwrapper.h>
+namespace rj = rapidjson;
+
+#include "../components/components.hpp"
 
 typedef enum
 {
     Continue,
-    Null
+    Null,
+    Block
 } Event;
+
+class Lesson;
 
 class JsonParser
 {
     public:
-        Event parseLesson(); //returns the continue key
+        JsonParser();
+        void parseLesson(Lesson& lesson); //returns the continue key
 
     private:
         int counter;
 
         //helper functions for parseLesson()
 
-        void updateText(); //updates the text of the scene
-        void createEntities(); //creates the appropiate entities
-        void parseAnim(); //parses and executes the animations
-        void parseTransition(); //parses and execute the transition between two screens
-        Event parseEvent(); //parses and returns the continue key
+        void updateText(rj::Document& doc, std::string& text); //updates the text of the scene
+        void loadRessources(Lesson& lesson); //load the ressources into the cache
+        void createEntities(rj::Document& doc, entt::DefaultRegistry* registry); //creates the appropiate entities
+        void parseAnim(rj::Document& doc, entt::DefaultRegistry* registry); //parses and executes the animations
+        void parseTransition(Lesson& lesson); //parses and execute the transition between two screens
+        void parseEvent(rj::Document& doc, Event& nextEvent); //parses and returns the continue key
 
         //helper functions for parseAnim()
 
