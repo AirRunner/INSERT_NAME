80938339a46e77004d93aabe3de99dbe8586172d
Vic8t <vic8.taillieu@gmail.com>
2019-05-27 16:15:00 +0200

    resized intro

--- scenes/menus/start_screen.cpp
+++ scenes/menus/start_screen.cpp
@@ -7,11 +7,11 @@ class LevelSelect;
 StartScreen::StartScreen()
 {
 	cacheManager = new CacheManager;
-    cacheManager->animations.load<animationLoader>(entt::HashedString{"credits"}, "data/assets/intro/", 3000);
+    cacheManager->animations.load<animationLoader>(entt::HashedString{"credits"}, "data/assets/intro", 3000);
 
     std::uint32_t entity = registry->create();
-    registry->assign<position>(entity, (float) GetScreenWidth()/2, (float) GetScreenHeight()/2);
-    registry->assign<anim>(entity, cacheManager->animations.handle(entt::HashedString{"credits"}), 0.f, (float) GetScreenWidth(), (float) GetScreenHeight());
+    registry->assign<position>(entity, (float) screenWidth/2, (float) screenHeight/2);
+    registry->assign<anim>(entity, cacheManager->animations.handle(entt::HashedString{"credits"}), 0.f, (float) screenWidth, (float) screenHeight);
 
     nextScene = new LevelSelect();
 }
@@ -29,7 +29,7 @@ Scene* StartScreen::handleEvents(float deltaTime)
     }
 
     if(IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || IsKeyPressed(KEY_RIGHT)){
-        return new Transition(this, nextScene, "left-right", "quad", "in", 800);
+        return new Transition(this, nextScene, "left-right", "sine", "out", 800);
     }
     
     return this;
